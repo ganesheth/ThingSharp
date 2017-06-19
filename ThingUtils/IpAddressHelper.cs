f26877718dcfe6064c0a1344cde86a6f04c2055b
charliekoerner <charlie.koerner.ext@siemens.com>
2017-06-19 17:16:13 -0500

    Updated Project
    - added auto address finding
    - updated .net to 4.6.2
    - changed Lifx Adapter code to work with updated Lifx Service
    - updated logging
    - added more supported Lifx properties

---
+++ ThingUtils/IpAddressHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace ThingSharp.Utils
+{
+    public class IpAddressHelper
+    {
+        //public static String IpAddress { get; set; }      
+  
+        public string GetLocalIPv4(NetworkInterfaceType type)
+        {
+            string foundIP = "";
+
+            foreach(NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if(item.NetworkInterfaceType == type && item.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach(UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                    {
+                        if(ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            foundIP = ip.Address.ToString();
+                        }
+                    }
+                }
+            }
+            return foundIP;            
+        }
+    }   
+}
