4fae01f9f36489e13b75f565400d5ace153090dd
charliekoerner <charlie.koerner.ext@siemens.com>
2017-06-19 17:24:12 -0500

    Updated Project
    - added auto address finding
    - updated .net to 4.6.2
    - changed Lifx Adapter code to work with updated Lifx Service
    - updated logging
    - added more supported Lifx properties

--- TestAdapter/LogicalAdapter/LIfxAdapter.cs
+++ TestAdapter/LogicalAdapter/LIfxAdapter.cs
@@ -7,37 +7,34 @@ using ThingSharp.Drivers;
 using ThingSharp.Server;
 using ThingSharp.Types;
 using ThingSharp.Utils;
+using RestAdapter;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ThingSharp.TestAdapter
 {
     class LifxAdapter : Adapter
     {
-        private LifxDriver driver;
+        private LifxDriver driver = null;
 
         public LifxAdapter(IPAddress localEndpoint)
         {
             driver = new LifxDriver(localEndpoint);
         }
+
         public override void Initialize(Uri baseUri)
         {
             Console.WriteLine("Initializing adapter..");
-            List<object> bulbs = driver.DiscoverBulbs(1);
-            int i = 1;
-            foreach(object b in bulbs)
-            {
-                GMS_OM_LED_LAMP lifxLamp = new GMS_OM_LED_LAMP(new Uri(baseUri, String.Format("lifx_{0}", i++)));
-                lifxLamp.Id = String.Format("lifx_{0}", i++);
-                lifxLamp.SubsystemContext = b;
-                lifxLamp.RemoveProperty("Brightness");
-                RaiseOnThingAdded(lifxLamp);
-            }
 
-            Console.WriteLine("Found {0} bulbs", bulbs.Count);
+            driver.DiscoverBulbs();
+            StartCheckingForNewBulbs(baseUri);
 
+            // TEST CODE //
             //Instances can be created from imported types (.net classes)
-            GMS_OM_LED_LAMP lightFromStaticType = new GMS_OM_LED_LAMP(new Uri(baseUri, "L1"));
-            lightFromStaticType.RemoveProperty("Brightness");
-            RaiseOnThingAdded(lightFromStaticType);
+            //LIFX_WIFI_LIGHT lightFromStaticType = new LIFX_WIFI_LIGHT(new Uri(baseUri, "L1"));
+            //lightFromStaticType.RemoveProperty("Brightness");
+            //RaiseOnThingAdded(lightFromStaticType);
 
             //Now, the variant of creating instances directly from RDF
             //Thing lightFromRdf = ThingModelParser.FromRDF(@"D:\Data\GMS\RESTDriverProject\Prototypes\GMS_OM.owl", "#GMS_OM_OSRAM_1", new Uri(baseUri, "L2"));
@@ -48,48 +45,153 @@ namespace ThingSharp.TestAdapter
             //RaiseOnThingAdded(lightFromRdf);
         }
 
+        void StartCheckingForNewBulbs(Uri baseUri)
+        {
+            var listenProc = new Thread(CheckForNewBulbs);
+            listenProc.Start(baseUri);
+        }
+
+        void CheckForNewBulbs(object ob)
+        {
+            try
+            {
+                Uri baseUri = ob as Uri;
+                int i = 1;
+
+                while (true)
+                {
+                    List<object> bulbs = driver.GetNewBulbs();
+
+                    foreach (object b in bulbs)
+                    {
+                        string id_string;
+                        string label = driver.GetBulbObjectLabel(b);
+                        if (String.IsNullOrEmpty(label))
+                        {
+                            id_string = String.Format("lifx_{0}", i++);
+                        }
+                        else
+                        {
+                            id_string = String.Format("lifx_{0}", label.Replace(' ', '_'));
+                        }                    
+
+                        LIFX_WIFI_LIGHT lifxLamp = new LIFX_WIFI_LIGHT(new Uri(baseUri, id_string));
+
+                        lifxLamp.Id = id_string;
+                        lifxLamp.SubsystemContext = b;
+
+                        RaiseOnThingAdded(lifxLamp);
+
+                        Console.WriteLine("Label: " + id_string + ":" + driver.GetBulbEndPoint(b));
+                    }
+
+                    if (bulbs.Count > 0)
+                    {
+                        Console.WriteLine("Found {0} new bulb(s)", bulbs.Count);
+                    }
+
+                    // Wait some amount of time before checking for new bulbs
+                    System.Threading.Thread.Sleep(10 * 1000);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
+        }
+
+        static int count = 1;
+
         public override object Read(Resource obj)
         {
+            Stopwatch sw = Stopwatch.StartNew();
+                       
+
             if (obj is Thing)
                 return obj;
 
+            object value = null;
+
             PropertyBase property = (PropertyBase)obj;
+
+            switch (property.Name)
+            {
+                case "Power":
+                    value = driver.GetBulbPower(property.Parent.SubsystemContext); 
+                    break;
+
+                case "Color":
+                    value = driver.GetBulbColor(property.Parent.SubsystemContext); 
+                    break;
+
+                case "Brightness":
+                    value = driver.GetBulbBrightness(property.Parent.SubsystemContext);
+                    break;
+
+                case "Saturation":
+                    value = driver.GetBulbSaturation(property.Parent.SubsystemContext);
+                    break;
+
+                case "Kelvin":
+                    value = driver.GetBulbKelvin(property.Parent.SubsystemContext); 
+                    break;
+
+                case "Label":
+                    value = driver.GetBulbObjectLabel(property.Parent.SubsystemContext);
+                    break;
+
+                default:
+                    break;
+            }
     
-            if (property.Name == "Switch")
-            {
-                bool status = driver.GetBulbStatus(property.Parent.SubsystemContext);
-                return status;
-            }
+            sw.Stop();
+            Console.WriteLine("GET:{0}\t -- TimeElapsed: {1}   ({2})", property.Name, sw.Elapsed, count++);
 
-            if (property.Name == "Color")
-            {
-                string color = driver.GetBulbColor(property.Parent.SubsystemContext);
-                return color;
-            }
-
-            return null;
+            return value;
         }
 
         public override object Write(Resource obj, object value)
         {
+            Stopwatch sw = Stopwatch.StartNew();
+
             if (obj is Thing)
                 return "Thing Description";
 
+            object returnStatus = null;
+
             PropertyBase property = (PropertyBase)obj;
 
-            if (property.Name == "Switch")
+            switch (property.Name)
             {
-                bool status = driver.SetBulbStatus(property.Parent.SubsystemContext,(bool)value);
-                return status;
+                case "Power": 
+                    returnStatus = driver.SetBulbPower(property.Parent.SubsystemContext, value); 
+                    break;
+
+                case "Color": 
+                    returnStatus = driver.SetBulbColor(property.Parent.SubsystemContext, value); 
+                    break;
+
+                case "Brightness": 
+                    returnStatus = driver.SetBulbBrightness(property.Parent.SubsystemContext, value); 
+                    break;
+
+                case "Saturation": 
+                    returnStatus = driver.SetBulbSaturation(property.Parent.SubsystemContext, value); 
+                    break;
+
+                case "Kelvin": 
+                    returnStatus = driver.SetBulbKelvin(property.Parent.SubsystemContext, value); 
+                    break;
+
+                default: 
+                    break;
             }
 
-            if (property.Name == "Color")
-            {
-                bool status = (bool)driver.SetBulbColor(property.Parent.SubsystemContext, (string)value);
-                return status;
-            }
+            sw.Stop();
+            Console.WriteLine("PUT:{0}\t -- TimeElapsed: {1}", property.Name, sw.Elapsed);
 
-            return null;
+            return returnStatus;
         }
     }
 }
--- TestAdapter/Program.cs
+++ TestAdapter/Program.cs
@@ -12,14 +12,39 @@ namespace ThingSharp.TestAdapter
     {
         static void Main(string[] args)
         {
-            IPAddress localEndpoint = IPAddress.Parse(args[0]);
-            int httpPort = int.Parse(args[1]);
+            IPAddress localEndpoint;
+            int httpPort;
+
+            if (args.Length > 0)
+            {
+                localEndpoint = IPAddress.Parse(args[0]);
+                httpPort = int.Parse(args[1]);
+                Console.WriteLine("EndPoint (manual): " + localEndpoint + ":" + httpPort.ToString());
+            }
+            else
+            {
+                // Find the IP Address of the Wireless Connection
+                IpAddressHelper IpHelper = new IpAddressHelper();
+                string ipString = IpHelper.GetLocalIPv4(System.Net.NetworkInformation.NetworkInterfaceType.Wireless80211);
+                if (!string.IsNullOrEmpty(ipString))
+                {
+                    localEndpoint = IPAddress.Parse(ipString);
+                    httpPort = 8080;
+                    Console.WriteLine("EndPoint (automatic): " + localEndpoint + ":8080");
+                }
+                else
+                {
+                    Console.WriteLine("Error: Could not automatically retreive the IP address for the Wireless adapter. \n\nPlease make sure the wireless adapter is turned on and connected to the network.");
+                    Console.Read();
+                    return;
+                }
+            }
             String httpEnpoint = String.Format("http://{0}:{1}/", localEndpoint.ToString(), httpPort);
 
             //First we choose what kind of protocol binding we want on top.
             //At the moment we have HTTP in stock, but expect WebSockets, CoAP etc next season.
             Uri baseUri = new Uri(httpEnpoint);
-            ProtocolBinding httpBinding = new HTTPBinding(new string[] { httpEnpoint});
+            ProtocolBinding httpBinding = new HTTPBinding(new string[] { httpEnpoint });
 
             //Next we create an Adapter. The Adapter is the stuff you will develop. It contains
             // a "logical adaption" and a driver layer.
